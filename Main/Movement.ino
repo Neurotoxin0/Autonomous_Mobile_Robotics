e14945bea235f4fd442c00c734688ceb2b00c729
Don <Xusi101066@163.com>
2022-09-23 16:49:23 -0400

    Update Movement.ino

--- Main/Movement.ino
+++ Main/Movement.ino
@@ -1,3 +1,11 @@
+#define turning_speed 180 // will turn how many angle in a sec, with speed 200
+
+void stop_movement()
+{
+  analogWrite(Left_Motor_PWM,0);
+  analogWrite(Right_Motor_PWM,0);
+}
+
 void move_front(int speed)
 {
   digitalWrite(Left_Motor_Ctrl,HIGH);
@@ -6,8 +14,26 @@ void move_front(int speed)
   analogWrite(Right_Motor_PWM,speed);
 }
 
-void stop_movement()
+void move_back(int speed)
 {
-  analogWrite(Left_Motor_PWM,0);
-  analogWrite(Right_Motor_PWM,0);
+  digitalWrite(Left_Motor_Ctrl,LOW);
+  analogWrite(Left_Motor_PWM,speed);
+  digitalWrite(Right_Motor_Ctrl,LOW);
+  analogWrite(Right_Motor_PWM,speed);
+}
+
+void left_turn(int angle)
+{
+  digitalWrite(Left_Motor_Ctrl,HIGH);
+  analogWrite(Left_Motor_PWM,speed);
+  digitalWrite(Right_Motor_Ctrl,HIGH);
+  analogWrite(Right_Motor_PWM,speed);
+}
+
+void right_turn(int angle)
+{
+  digitalWrite(Left_Motor_Ctrl,HIGH);
+  analogWrite(Left_Motor_PWM,speed);
+  digitalWrite(Right_Motor_Ctrl,HIGH);
+  analogWrite(Right_Motor_PWM,speed);
 }
