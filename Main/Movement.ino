9928a94904159884668caf9ccc01c6155600119d
Don <Xusi101066@163.com>
2022-09-24 10:19:08 +0800

    Update Movement.ino

--- Main/Movement.ino
+++ Main/Movement.ino
@@ -1,26 +1,26 @@
-#define Turning_Speed 200
+const int Turning_Speed = 200;
 // 350~400 ms for ~45 degrees with speed 200
 
 void stop_movement()
 {
-  analogWrite(Left_Motor_PWM,0);
-  analogWrite(Right_Motor_PWM,0);
+  analogWrite(Left_Motor_PWM, 0);
+  analogWrite(Right_Motor_PWM, 0);
 }
 
 void move_front(int speed)
 {
-  digitalWrite(Left_Motor_Ctrl,HIGH);
-  analogWrite(Left_Motor_PWM,speed);
-  digitalWrite(Right_Motor_Ctrl,HIGH);
-  analogWrite(Right_Motor_PWM,speed);
+  digitalWrite(Left_Motor_Ctrl, HIGH);
+  analogWrite(Left_Motor_PWM, speed);
+  digitalWrite(Right_Motor_Ctrl, HIGH);
+  analogWrite(Right_Motor_PWM, speed);
 }
 
 void move_back(int speed)
 {
-  digitalWrite(Left_Motor_Ctrl,LOW);
-  analogWrite(Left_Motor_PWM,speed);
-  digitalWrite(Right_Motor_Ctrl,LOW);
-  analogWrite(Right_Motor_PWM,speed);
+  digitalWrite(Left_Motor_Ctrl, LOW);
+  analogWrite(Left_Motor_PWM, speed);
+  digitalWrite(Right_Motor_Ctrl, LOW);
+  analogWrite(Right_Motor_PWM, speed);
 }
 
 void left_turn(int time)
