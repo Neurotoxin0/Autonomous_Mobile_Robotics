34ff5dda40aebae567a93fa82a2122a1ce81bb1b
Don <Xusi101066@163.com>
2022-09-25 06:51:24 +0800

    Add debug option

--- Main/Ultra_Sonic.ino
+++ Main/Ultra_Sonic.ino
@@ -1,8 +1,12 @@
-long ultra_sonic_get_distance() 
+long ultra_sonic_get_distance(bool debug) 
 { 
-  long result = sr04.Distance();
-  Serial.print("Distance: ");
-  Serial.print(result);
-  Serial.print("\n");
-  return result; 
+  if (debug)
+  {
+    long result = sr04.Distance();
+    Serial.print("Distance: ");
+    Serial.print(result);
+    Serial.print("\n");
+    return result; 
+  }
+  else { return sr04.Distance(); }
 }
