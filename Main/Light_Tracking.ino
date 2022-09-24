7ab174baa39e700e089a1323cfcd92e23c7acfd9
Don <Xusi101066@163.com>
2022-09-25 05:53:00 +0800

    Create Light_Tracking.ino

---
+++ Main/Light_Tracking.ino
@@ -0,0 +1,14 @@
+int left_light, right_light;
+
+bool following_light() { return (digitalRead(Left_Light_Sensor) == 1) && (digitalRead(Right_Light_Sensor) == 1); }
+
+void adjust_light_following()
+{
+  left_light = digitalRead(Left_Light_Sensor);
+  right_light = digitalRead(Right_Light_Sensor);
+  
+  if      ( (left_light == 1) && (right_light == 0) ) { left_turn(Slow_Turning_Speed, 100); }
+  else if ( (left_light == 0) && (right_light == 1))  { right_turn(Slow_Turning_Speed, 100); }
+  // completly off-track OR adjust (above) is complete
+  else { stop_movement(); }
+}
