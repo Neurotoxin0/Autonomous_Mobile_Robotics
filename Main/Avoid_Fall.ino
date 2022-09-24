4c6c5186c2f94e0b5a3367d25c1c95b0fc29599f
Don <Xusi101066@163.com>
2022-09-25 05:52:54 +0800

    Update Avoid_Fall.ino

--- Main/Avoid_Fall.ino
+++ Main/Avoid_Fall.ino
@@ -1,12 +1,13 @@
 bool fall_detected()
 {
-  //return front_central_fall_detection() || digitalRead(Front_Left_Sensor) || digitalRead(Front_Right_Sensor) || digitalRead(Back_Left_Sensor) || digitalRead(Back_Right_Sensor);
+  //return (front_central_fall_detection()) || (digitalRead(Left_Edge_Sensor)) || (digitalRead(Right_Edge_Sensor));
   return front_central_fall_detection();
 }
 
 
 void avoid_fall()
 {
+  //if ( digitalRead(Left_Edge_Sensor)() && digitalRead(Right_Edge_Sensor) )
   if (front_central_fall_detection())
   {
       move_back(200);
@@ -14,31 +15,19 @@ void avoid_fall()
       right_turn(Default_Turning_Speed, 175);
   }
   /*
-  else if (digitalRead(Front_Left_Sensor))
+  else if (digitalRead(Left_Edge_Sensor))
   {
       move_back(200);
       delay(100);
       left_turn(Default_Turning_Speed, 175);
   }
-  else if (digitalRead(Front_Right_Sensor))
+  else if (digitalRead(Right_Edge_Sensor))
   {
       move_back(200);
       delay(100);
       right_turn(Default_Turning_Speed, 175);
-  }
-  else if (digitalRead(Back_Left_Sensor))
-  {
-      move_back(200);
-      delay(100);
-      right_turn(Default_Turning_Speed, 175);
-  }
-  else if (digitalRead(Back_Right_Sensor))
-  {
-      move_back(200);
-      delay(100);
-      left_turn(Default_Turning_Speed, 175);
   }
   */
 }
 
-bool front_central_fall_detection() { return digitalRead(Front_Central_Left_Sensor) || digitalRead(Front_Central_Middle_Sensor) || digitalRead(Front_Central_Right_Sensor); }
+bool front_central_fall_detection() { return digitalRead(Left_Line_Sensor) || digitalRead(Central_Line_Sensor) || digitalRead(Right_Line_Sensor); }
