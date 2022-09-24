6273b800e3a341505d78598b88d42cb54f2d364e
Don <Xusi101066@163.com>
2022-09-24 10:09:20 -0400

    Update Avoid_Fall.ino

--- Main/Avoid_Fall.ino
+++ Main/Avoid_Fall.ino
@@ -1,11 +1,37 @@
-int fall_detection()
+void avoid_fall()
 {
-  if (front_central_fall_detection()) return 2;
-  //else if (digitalRead(Front_Left_Sensor)) return 1;
-  //else if (digitalRead(Front_Right_Sensor)) return 3;
-  //else if (digitalRead(Back_Left_Sensor)) return 4;
-  //else if (digitalRead(Back_Right_Sensor)) return 5;
-  return 0;
+  if (front_central_fall_detection())
+  {
+      move_back(80); // @params: speed; 80 < x < 255
+      delay(500);
+      right_turn(350);// @params: time; x ms
+  }
+  /*
+  else if (digitalRead(Front_Left_Sensor))
+  {
+      move_back(80);
+      delay(500);
+      left_turn(350);
+  }
+  else if (digitalRead(Front_Right_Sensor))
+  {
+      move_back(80);
+      delay(500);
+      right_turn(350);
+  }
+  else if (digitalRead(Back_Left_Sensor))
+  {
+      move_front(80);
+      delay(500);
+      right_turn(350);
+  }
+  else if (digitalRead(Back_Right_Sensor))
+  {
+      move_front(80);
+      delay(500);
+      left_turn(350);
+  }
+  */
 }
 
 bool front_central_fall_detection() { return digitalRead(Front_Central_Left_Sensor) || digitalRead(Front_Central_Middle_Sensor) || digitalRead(Front_Central_Right_Sensor); }
