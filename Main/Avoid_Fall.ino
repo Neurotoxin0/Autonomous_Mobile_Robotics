0e24433588df4c4e58b6ecc87195cf9e873c9493
Don <Xusi101066@163.com>
2022-09-24 10:42:12 +0800

    Update Avoid_Fall.ino

--- Main/Avoid_Fall.ino
+++ Main/Avoid_Fall.ino
@@ -1,5 +1,11 @@
-
-int front_fall_detection()
+int fall_detection()
 {
-  return digitalRead(Front_Left_Sensor) || digitalRead(Front_Middle_Sensor) || digitalRead(Front_Right_Sensor);
+  if (front_central_fall_detection()) return 2;
+  //else if (digitalRead(Front_Left_Sensor)) return 1;
+  //else if (digitalRead(Front_Right_Sensor)) return 3;
+  //else if (digitalRead(Back_Left_Sensor)) return 4;
+  //else if (digitalRead(Back_Right_Sensor)) return 5;
+  return 0;
 }
+
+bool front_central_fall_detection() { return digitalRead(Front_Central_Left_Sensor) || digitalRead(Front_Central_Middle_Sensor) || digitalRead(Front_Central_Right_Sensor); }
