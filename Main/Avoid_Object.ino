ba1b4d4434b9a9a1d6fbeda7e5b729523742feb5
Don <Xusi101066@163.com>
2022-09-25 00:07:10 +0800

    Update Avoid_Object.ino

--- Main/Avoid_Object.ino
+++ Main/Avoid_Object.ino
@@ -1,28 +1,23 @@
-long Ultrasonic_Central_Distance, Ultrasonic_Left_Distance, Ultrasonic_Right_Distance;
+long left_distance, right_distance;
 
 bool collision_detected() { return ultra_sonic_get_distance() < 10; }
 
 void avoid_object()
 {
-  Ultrasonic_Central_Distance = ultra_sonic_get_distance();
-  
-  if (Ultrasonic_Central_Distance < 10)
-  {
-    move_back(200);
-    delay(100);
-    stop_movement();
+  move_back(200);
+  delay(100);
+  stop_movement();
     
-    // Measure left side distance
-    servo_to_angle(180);
-    Ultrasonic_Left_Distance = ultra_sonic_get_distance();
+  // Measure left side distance
+  servo_to_angle(180);
+  left_distance = ultra_sonic_get_distance();
     
-    // Measure right side distance
-    servo_to_angle(0);  // turn to right
-    Ultrasonic_Right_Distance = ultra_sonic_get_distance();
+  // Measure right side distance
+  servo_to_angle(0);  // turn to right
+  right_distance = ultra_sonic_get_distance();
     
-    if (Ultrasonic_Left_Distance > Ultrasonic_Right_Distance) { left_turn(Default_Turning_Speed, 350); }
-    else                                                      { right_turn(Default_Turning_Speed, 350); }
+  if (left_distance > right_distance) { left_turn(Default_Turning_Speed, 350); }
+  else                                { right_turn(Default_Turning_Speed, 350); }
     
-    servo_to_angle(90);
-  }
+  servo_to_angle(90);
 }
