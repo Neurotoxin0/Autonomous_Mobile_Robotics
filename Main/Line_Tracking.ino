f34940dde436c208cea13fd7655285c8c082264d
Don <Xusi101066@163.com>
2022-09-25 05:52:58 +0800

    Update Line_Tracking.ino

--- Main/Line_Tracking.ino
+++ Main/Line_Tracking.ino
@@ -1,15 +1,14 @@
 int left_line, right_line;
-const int Slow_Turning_Speed = 80;
 
-bool on_track() { return digitalRead(Front_Central_Middle_Sensor) == 1; }
+bool following_line() { return digitalRead(Central_Line_Sensor) == 1; }
 
-void back_on_track()
+void adjust_line_tracking()
 {
-  left_line = digitalRead(Front_Central_Left_Sensor);
-  right_line = digitalRead(Front_Central_Right_Sensor);
+  left_line = digitalRead(Left_Line_Sensor);
+  right_line = digitalRead(Right_Line_Sensor);
   
-  if( (left_line == 1) && (right_line == 0) ){ left_turn(Slow_Turning_Speed, 100); }
-  else if( (left_line == 0) && (right_line == 1)) { right_turn(Slow_Turning_Speed, 100); }
+  if      ( (left_line == 1) && (right_line == 0) ) { left_turn(Slow_Turning_Speed, 100); }
+  else if ( (left_line == 0) && (right_line == 1))  { right_turn(Slow_Turning_Speed, 100); }
   // completly off-track OR adjust (above) is complete
   else { stop_movement(); }
 }
