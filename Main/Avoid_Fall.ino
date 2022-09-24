86842c4276c787b7a527a07574694205dedd3289
Don <Xusi101066@163.com>
2022-09-24 10:46:24 -0400

    Update Avoid_Fall.ino

--- Main/Avoid_Fall.ino
+++ Main/Avoid_Fall.ino
@@ -2,34 +2,34 @@ void avoid_fall()
 {
   if (front_central_fall_detection())
   {
-      move_back(80); // @params: speed; 80 < x < 255
-      delay(500);
-      right_turn(350);// @params: time; x ms
+      move_back(200); // @params: speed; 80 < x < 255
+      delay(100);
+      right_turn(Turning_Speed, 350);// @params: time; x ms
   }
   /*
   else if (digitalRead(Front_Left_Sensor))
   {
-      move_back(80);
-      delay(500);
-      left_turn(350);
+      move_back(200);
+      delay(100);
+      left_turn(Turning_Speed, 350);
   }
   else if (digitalRead(Front_Right_Sensor))
   {
-      move_back(80);
-      delay(500);
-      right_turn(350);
+      move_back(200);
+      delay(100);
+      right_turn(Turning_Speed, 350);
   }
   else if (digitalRead(Back_Left_Sensor))
   {
-      move_front(80);
-      delay(500);
-      right_turn(350);
+      move_back(200);
+      delay(100);
+      right_turn(Turning_Speed, 350);
   }
   else if (digitalRead(Back_Right_Sensor))
   {
-      move_front(80);
-      delay(500);
-      left_turn(350);
+      move_back(200);
+      delay(100);
+      left_turn(Turning_Speed, 350);
   }
   */
 }
