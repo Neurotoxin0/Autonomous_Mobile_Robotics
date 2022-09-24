8537f942162c8ea8abd24a2b9cf0d0df22fd2279
Don <Xusi101066@163.com>
2022-09-25 07:14:39 +0800

    Update Line_Tracking.ino

--- Main/Line_Tracking.ino
+++ Main/Line_Tracking.ino
@@ -1,16 +1,33 @@
 int left_line, right_line;
 
-bool following_line() { return digitalRead(Central_Line_Sensor) == 1; }
+bool following_line() { return digitalRead(Central_Line_Sensor) == 0; }
 
-void adjust_line_tracking()
+bool adjust_line_tracking()
 {
   Serial.print("adjust_line_tracking()\n");
-  
-  left_line = digitalRead(Left_Line_Sensor);
-  right_line = digitalRead(Right_Line_Sensor);
-  
-  if      ( (left_line == 1) && (right_line == 0) ) { left_turn(Slow_Turning_Speed, 100); }
-  else if ( (left_line == 0) && (right_line == 1))  { right_turn(Slow_Turning_Speed, 100); }
-  // completly off-track OR adjust (above) is complete
-  else { stop_movement(); }
+  tries = 0;
+
+  while ( not following_line() && tries < 150 )
+  {
+    left_line = digitalRead(Left_Line_Sensor);
+    right_line = digitalRead(Right_Line_Sensor);
+    tries++;
+    Serial.print("tries: ");
+    Serial.print(tries);
+    Serial.print("\n");
+    
+    if (following_line())  // exit case
+    {
+      stop_movement();
+      Serial.print("Adjusted\n"); 
+      return 1;
+    }
+    else if ( left_line == 1 && right_line == 0 ) { left_turn(Slow_Turning_Speed, 100); }
+    else if ( left_line == 0 && right_line == 1 ) { right_turn(Slow_Turning_Speed, 100); }
+    else  // random tries
+    { 
+      left_turn(Slow_Turning_Speed, 100);
+    }
+  }
+  return 0;
 }
