52ce37247c8db034ea399d6afc78b6e3b49fb642
Don <Xusi101066@163.com>
2022-09-24 10:19:06 +0800

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,8 +4,6 @@ Ruoling Yu 500976267
 Xinyu Ma 500943173
 */
 
-#include "SR04.h" //ultrasonic sensor lib
-
 // motor control and pwn pins
 #define Left_Motor_Ctrl 4
 #define Left_Motor_PWM 5
@@ -13,11 +11,12 @@ Xinyu Ma 500943173
 #define Right_Motor_PWM 6
 
 // fall & line tracking sensor
-#define Front_left_sensor 11
-#define Front_middle_snsor 7
-#define Front_right_snsor 8
+#define Front_Left_Sensor 11
+#define Front_Middle_Sensor 7
+#define Front_Right_Sensor 8
 
 // ultrasonic sensor
+#include "SR04.h" //ultrasonic sensor lib
 #define Ultrasonic_Receive 12
 #define Ultrasonic_Send 13
 
@@ -36,28 +35,27 @@ void setup()
   pinMode(Right_Motor_PWM, OUTPUT);
 
   // fall & line tracking sensor
-  pinMode(Front_left_sensor,INPUT);
-  pinMode(Front_middle_snsor,INPUT);
-  pinMode(Front_right_snsor,INPUT);
+  pinMode(Front_Left_Sensor, INPUT);
+  pinMode(Front_Middle_Sensor, INPUT);
+  pinMode(Front_Right_Sensor, INPUT);
 
   // ultrasonic sensor
   //pinMode(Ultrasonic_Send, INPUT);
   //pinMode(Ultrasonic_Receive, OUTPUT);
   
-
   // servo
-  //servopulse(Servo_Pin,90);// the angle of servo is 90 degree
+  //servopulse(Servo_Pin, 90);// the angle of servo is 90 degree
 }
 
 void loop()
 {
-  move_front(80);
+  move_front(80); // @params: speed; 80 < x < 255
   
   if (front_fall_detection())
   {
-    move_back(80); delay(500);
-    left_turn(350);
-    //stop_movement();
+    move_back(80); // @params: speed; 80 < x < 255
+    delay(500);
+    left_turn(350);// @params: time; x ms
   }
 
   
