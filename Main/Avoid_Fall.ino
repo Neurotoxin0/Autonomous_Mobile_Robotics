3a65d0368e34b750be248ebf6936369f8c00d7cc
Don <Xusi101066@163.com>
2022-09-26 02:59:46 +0800

    Update Avoid_Fall.ino

--- Main/Avoid_Fall.ino
+++ Main/Avoid_Fall.ino
@@ -1,32 +1,66 @@
 bool fall_detected()
-{ 
-  return (digitalRead(Left_Edge_Sensor)) || (digitalRead(Right_Edge_Sensor));
-  //return front_central_fall_detection(); 
+{
+  return front_fall_detected() || back_fall_detected();
 }
+
+bool front_fall_detected()
+{ 
+  return digitalRead(Front_Central_Edge_Sensor) || digitalRead(Front_Left_Edge_Sensor) || digitalRead(Front_Right_Edge_Sensor);
+}
+
+bool back_fall_detected()
+{ 
+  return digitalRead(Back_Left_Edge_Sensor) || digitalRead(Back_Right_Edge_Sensor);
+}
+
 
 void avoid_fall()
 {
   Serial.print("avoid_fall()\n");
   
-  if ( digitalRead(Left_Edge_Sensor) && digitalRead(Right_Edge_Sensor) )
-  //if (front_central_fall_detection())
+  // both
+  if ( front_fall_detected() && back_fall_detected() ) { right_turn(Default_Turning_Speed, 300); }
+
+  // front
+  else if ( digitalRead(Front_Left_Edge_Sensor) && digitalRead(Front_Right_Edge_Sensor) )
+  //else if (digitalRead(Front_Central_Edge_Sensor))
   {
       move_back(200);
       delay(100);
       right_turn(Default_Turning_Speed, 300);
   }
-  else if (digitalRead(Left_Edge_Sensor))
+  else if (digitalRead(Front_Left_Edge_Sensor))
   {
       move_back(200);
       delay(100);
       right_turn(Default_Turning_Speed, 175);
       
   }
-  else if (digitalRead(Right_Edge_Sensor))
+  else if (digitalRead(Front_Right_Edge_Sensor))
   {
       move_back(200);
       delay(100);
       left_turn(Default_Turning_Speed, 175);
+  }
+
+  // back
+  else if (digitalRead(Back_Left_Edge_Sensor) && digitalRead(Back_Right_Edge_Sensor)) 
+  {
+      move_front(200);
+      delay(100);
+      stop_movement();
+  }
+  else if (digitalRead(Back_Left_Edge_Sensor)) 
+  {
+      move_front(200);
+      delay(100);
+      left_turn(Default_Turning_Speed, 175);
+  }
+  else if (digitalRead(Back_Right_Edge_Sensor))
+  {
+      move_front(200);
+      delay(100);
+      right_turn(Default_Turning_Speed, 175);
   }
 }
 
