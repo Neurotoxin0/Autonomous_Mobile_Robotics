f4c171b9fd4e0af888539e6f5e2689565e717420
Don <Xusi101066@163.com>
2022-09-27 03:11:04 +0800

    Create Motor_Speed_Adjust.ino

---
+++ Main/Motor_Speed_Adjust.ino
@@ -0,0 +1,21 @@
+void motor_speed_adjust()
+{
+  move_front(Base_Speed);
+  Serial.print("\nBase Speed: ");
+  Serial.print(Base_Speed);
+  Serial.print("\n");
+  
+  while (digitalRead(Back_Central_Edge_Sensor) == 1)
+  {
+    Base_Speed += 5;
+    Serial.print("Adjust Base Speed to: ");
+    Serial.print(Base_Speed);
+    Serial.print("\n");
+    move_front(Base_Speed);
+    delay(500);
+  }
+  
+  Serial.print("FINAL Base Speed: ");
+  Serial.print(Base_Speed);
+  Serial.print("\n");
+}
