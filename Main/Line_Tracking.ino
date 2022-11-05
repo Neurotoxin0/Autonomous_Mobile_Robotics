d7267c74c7d7fd3a376675f39e51f09a6e193843
Don <Xusi101066@163.com>
2022-11-04 21:25:07 -0400

    Update Line_Tracking.ino

--- Main/Line_Tracking.ino
+++ Main/Line_Tracking.ino
@@ -1,8 +1,7 @@
-bool found_line() { return digitalRead(Left_Line_Sensor) || digitalRead(Right_Line_Sensor) || partial_central_line_sensor(); }
-bool end_of_line() { return digitalRead(Left_Line_Sensor) == 0 && digitalRead(Right_Line_Sensor) == 0 && not partial_central_line_sensor(); }
-bool on_track() { return digitalRead(Left_Line_Sensor) == 0 && digitalRead(Right_Line_Sensor) == 0 && partial_central_line_sensor(); }
-bool partial_central_line_sensor()  { return digitalRead(Central_Line_Sensor) == 1 || digitalRead(Central_Left_Line_Sensor) == 1 || digitalRead(Central_Right_Line_Sensor) == 1; }
-bool all_central_line_sensor()      { return digitalRead(Central_Line_Sensor) == 1 && digitalRead(Central_Left_Line_Sensor) == 1 && digitalRead(Central_Right_Line_Sensor) == 1; }
+bool found_line() { return digitalRead(Left_Line_Sensor) || digitalRead(Right_Line_Sensor) || on_track(); }
+bool end_of_line() { return not digitalRead(Left_Line_Sensor) && not digitalRead(Right_Line_Sensor) && not on_track(); }
+bool on_track() { return digitalRead(Central_Line_Sensor) == 1 || digitalRead(Central_Left_Line_Sensor) == 1 || digitalRead(Central_Right_Line_Sensor) == 1; }
+//bool all_central_line_sensor()      { return digitalRead(Central_Line_Sensor) == 1 && digitalRead(Central_Left_Line_Sensor) == 1 && digitalRead(Central_Right_Line_Sensor) == 1; }
 
 void adjust_line_tracking()
 {
@@ -12,11 +11,13 @@ void adjust_line_tracking()
   if ( digitalRead(Central_Left_Line_Sensor) == 0 && digitalRead(Central_Right_Line_Sensor) == 1 ) 
   { 
     right_turn(Slow_Turning_Speed, 100, true); 
+    // TODO: move front a bit until detect a line
   }
   
   if ( digitalRead(Central_Left_Line_Sensor) == 1 && digitalRead(Central_Right_Line_Sensor) == 0 ) 
   { 
     left_turn(Slow_Turning_Speed, 100, true); 
+    // TODO: move front a bit until detect a line
   }
   
   // Outer Sensors
