050835f0e6438435ccafa151bb6bb85bd0a0fea1
Don <Xusi101066@163.com>
2022-11-09 15:00:15 -0500

    Fuck untra_sonic !

--- Main/Line_Tracking.ino
+++ Main/Line_Tracking.ino
@@ -1,27 +1,35 @@
-int Central, Central_Left, Central_Right, Outer_Left, Outer_Right;
-
+int Central, Central_Left, Central_Right, Outer_Left, Outer_Right, detection_count, back_count;
 bool on_track()   { return digitalRead(Central_Line_Sensor) && digitalRead(Central_Left_Line_Sensor) && digitalRead(Central_Right_Line_Sensor); }
 bool found_line() { return digitalRead(Central_Line_Sensor) || digitalRead(Central_Left_Line_Sensor) || digitalRead(Central_Right_Line_Sensor); }
-//bool end_of_line() { return not found_line(); }
+bool on_the_line(){ return digitalRead(Central_Line_Sensor) || digitalRead(Central_Left_Line_Sensor) || digitalRead(Central_Right_Line_Sensor) || digitalRead(Left_Line_Sensor) || digitalRead(Right_Line_Sensor); };
 
 void line_following()
 {
+  detection_count = 0;
+  back_count = 0;
+  
   while (true)
   {
-    //if (fall_detected() || collision_detected()) { return ; }
-    if (fall_detected()) { return ; }
-    //if(central_sr04.Distance() <= Safety_Distance) { return ; }
-    
+    if (detection_count == 5)
+    {
+      //if (fall_detected() || collision_detected()) { return ; }
+      if (fall_detected()) { return ; }
+      detection_count = 0;
+    }
+    //Serial.print("\tdetection\t");
     Central       = digitalRead(Central_Line_Sensor);
     Central_Left  = digitalRead(Central_Left_Line_Sensor);
     Central_Right = digitalRead(Central_Right_Line_Sensor);
     Outer_Left    = digitalRead(Left_Line_Sensor);
     Outer_Right   = digitalRead(Right_Line_Sensor);
-
-    if(Central == 1)
-    {
-      move_front(Base_Speed);
-    }
+    
+    /*Serial.print(Central);
+    Serial.print(Central_Left);
+    Serial.print(Central_Right);
+    Serial.print(Outer_Left);
+    Serial.print(Outer_Right);
+    Serial.print("\n");*/
+    if(Central == 1) { move_front(Base_Speed); }
     else
     {
       if((Central_Left == 1) && (Central_Right == 0))
@@ -40,18 +48,19 @@ void line_following()
       {
         right_turn(Default_Turning_Speed, 0);
       }
-      else if (Central || Central_Left || Central_Right || Outer_Left || Outer_Right)
-      {
-          move_back(200,50);
-      }
       else
       {
-        stop_movement();
-        //Serial.print("Exit Line Following\n");
-        return ;
+        if (back_count <= 15) 
+        { 
+          move_back(200, 25); 
+          back_count++; 
+          if (not on_the_line()) { return ; }
+        }
+        else { stop_movement(); }
       }
-     }
-    
-    
+    }
+  /*Serial.print("Count: ");
+  Serial.print(count);*/
+  detection_count ++;  
   }
 }
